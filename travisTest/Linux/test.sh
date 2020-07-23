1895d1cb695071711db3cd5fb4667a2f9022ca5c
AnzoDK <antonr@live.dk>
2020-07-23 15:44:36 +0200

    Made scripts executeable

--- travisTest/Linux/test.sh
+++ travisTest/Linux/test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #Linux
-mkdir build
-cd build
-make ../../.. test
+make test
 mkdir audio
 cd audio
 curl "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" --output mp3.mp3
 curl "https://file-examples-com.github.io/uploads/2017/11/file_example_WAV_10MG.wav" --output wav.wav
-curl "https://file-examples-com.github.io/uploads/2017/11/file_example_OOG_5MG.ogg" -- output ogg.ogg
+curl "https://file-examples-com.github.io/uploads/2017/11/file_example_OOG_5MG.ogg" --output ogg.ogg
 cd ..
 rptest --async --mp3
 rptest --mp3

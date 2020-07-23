0c78c61095d6a6c4af281916550c2f80ea872018
AnzoDK <antonr@live.dk>
2020-07-23 15:32:20 +0200

    Fixed and added travis tests

--- travisTest/Win/test.sh
+++ travisTest/Win/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Linux
+#Windows
 mkdir build
 cd build
-make ../../.. test
+make ../../.. test CXX=x86_64-w64-mingw32-g++
 mkdir audio
 cd audio
 curl "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" --output mp3.mp3
@@ -13,3 +13,4 @@ rptest --async --mp3
 rptest --mp3
 rptest --wav
 rptest --ogg
+

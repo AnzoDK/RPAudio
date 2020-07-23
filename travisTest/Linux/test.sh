dd596c2c3e3324e00643558b194f2f0686b04b75
AnzoDK <antonr@live.dk>
2020-07-23 15:22:23 +0200

    Added linux test for TravisCI

---
+++ travisTest/Linux/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir build
+cd build
+make ../../.. test
+mkdir audio
+cd audio
+curl "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" --output mp3.mp3
+curl "https://file-examples-com.github.io/uploads/2017/11/file_example_WAV_10MG.wav" --output wav.wav
+curl "https://file-examples-com.github.io/uploads/2017/11/file_example_OOG_5MG.ogg" -- output ogg.ogg
+cd ..
+rptest --async --mp3
+rptest --mp3
+rptest --wav
+rptest --ogg
